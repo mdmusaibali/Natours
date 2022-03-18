6650a216a9f432326990601a97f287eddf8acac7
Mohammed Musaib Ali <mdmusaibali@gmail.com>
2022-03-18 13:58:39 +0530

    Adding build process

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@
 const navButton = document.querySelector(".navigation__button");
 const navList = document.querySelector(".navigation__list");
 const navCheck = document.querySelector(".navigation__checkbox");
+const popup = document.querySelector(".popup");
+const popupContent = document.querySelector(".popup__content");
 navList.addEventListener("click", function (e) {
   if (!e.target.classList.contains("navigation__link")) return;
   e.target.addEventListener("click", function (ev) {
@@ -11,4 +13,9 @@ navList.addEventListener("click", function (e) {
   console.log(document.querySelector(id));
   document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   if ((navCheck.checked = true)) navCheck.checked = false;
+});
+
+popup.addEventListener("click", function (e) {
+  if (e.target.parentNode != document.body) return;
+  window.location = "http://127.0.0.1:8080/#section__tours";
 });
